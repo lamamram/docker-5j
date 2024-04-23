bfa6cf2a010405a749f437c92f69d568c844d913
mlamamra <mlamamra@dawan.fr>
2024-04-23 12:05:10 +0200

    réseaux : shell idempotent avec docker ps -q -f

--- stack_php/stack_php.sh
+++ stack_php/stack_php.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-docker ps -a | grep stack_php_nginx
-# $? le code de retour  de la commande dernière
-if [ $? -eq 0 ]; then
-    docker rm -f stack_php_nginx
-fi
+# test de l'existence des conteneurs si c'est vrai je supprime les conteneurs
+# -q: affiche uniquement les identifiants
+[[ -z $(docker ps -aq --filter "name=stack_php_*") ]] || docker rm -f $(docker ps -aq -f "name=stack_php_*")
+# # $? le code de retour  de la commande dernière
+# if [ $? -eq 0 ]; then
+#     docker rm -f stack_php_nginx
+# fi
 
 docker run \
        --name stack_php_nginx \
        -d --restart unless-stopped \
        -p 192.168.1.30:8080:80 \
        nginx:1.25
+
+
+docker run \
+       --name stack_php_php \
+       -d --restart unless-stopped \
+       bitnami/php-fpm:8.2-debian-12
